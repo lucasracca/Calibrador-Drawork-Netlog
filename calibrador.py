9814d70fdd66fb45c3bba2947c561ab9b2ba12de
lucas <lucasraccar@hotmail.com>
2020-10-03 17:43:16 -0300

    Comienzo del proyecto

---
+++ calibrador.py
@@ -0,0 +1,251 @@
+import sys
+import datetime
+import time
+
+from comunicacion import *
+
+###############################################################################
+def EnviarGeometria():
+     inPpr = input('Ingrese PPR\n')
+     try:
+         ppr = int(inPpr)
+     except ValueError:
+         print("Ppr no es un entero")
+         return
+
+     inDiametro = input('Ingrese Diametro del tambor\n')
+     try:
+        diametro = float(inDiametro)
+     except ValueError:
+         print("Diametro tambor no es un real")
+         return
+
+     inCable = input('Ingrese Diametro del cable\n')
+     try:
+         cable = float(inCable)
+     except ValueError:
+        print("Diametro cable no es un real")
+        return
+
+     inAparejo = input('Ingrese Factor de aparejo\n')
+     try:
+        aparejo = float(inAparejo)
+     except ValueError:
+        print("Lineas del aparejo no es un real")
+        return
+
+     inHpc = input('Ingrese hiladas por capa\n')
+     try:
+        hpc = int(inHpc)
+     except ValueError:
+        print("Hpc no es un entero")
+        return
+
+     rta = comm.setGeometry(str(ppr), str(diametro), str(cable), str(hpc), str(aparejo))
+     print(rta[0])
+     print(hora())
+
+###############################################################################
+def EnviarAjuste():
+    inAltura =input('Ingrese Altura Inicial\n')
+    try:
+         altura = float(inAltura)
+    except ValueError:
+        print("Altura de referencia no es un real")
+        return
+    inCapas = input('Ingrese Capas completas\n')
+    try:
+        capas = int(inCapas)
+    except ValueError:
+        print("Capas completas no es un entero")
+        return
+    inHiladas = input('Ingrese Hiladas en ultima capa\n')
+    try:
+        hiladas = int(inHiladas)
+    except ValueError:
+        print("Cantidad de Hiladas Última capa no es un entero")
+        return
+    rta =comm.adjust(str(capas), str(hiladas), str(altura))
+    print(rta[0])
+    print(hora())
+
+###############################################################################
+def EnviarPyM():
+    inM = input('Ingrese Pendiente M\n')
+    inP = input('Ingrese Punto 1\n')
+    try:
+        m = float(inM)
+    except ValueError:
+        print("Pendiente M no es un real")
+        return
+    try:
+        n = float(inP)
+    except ValueError:
+        print("Punto 1 no es un real")
+        return
+
+    rta = comm.setRef2(str(n))
+    print(rta[0])
+    print(hora())
+    rtastr = ','.join(rta)
+
+    if 'Operación Realizada.' in rtastr :
+        rta = comm.setM(str(m / 4))
+        print(rta[0])
+        print(hora())
+
+###############################################################################
+def EnviarPunto1():
+    inPto1 =input('Ingrese Altura Referencia 1\n')
+    try:
+        pto1 = float(inPto1)
+    except ValueError:
+        print("Altura Referencia 1 no es un real")
+        return
+    rta = comm.setRef2(str(inPto1))
+    print(rta[0])
+    print(hora())
+
+###############################################################################
+
+def EnviarPunto2():
+    inPto2 = input('Ingrese Altura Referencia 2\n')
+    try:
+        pto2 = float(inPto2)
+    except ValueError:
+        print("Altura Referencia 2 no es un real")
+        return
+    rta = comm.setRef1(str(inPto2))
+    rtastr = ','.join(rta)
+    if 'ERROR X2-X1 es igual a 0' in rtastr:
+        print("El punto 2 se encuentra en la misma posicion que el punto 1" )
+    else:
+        print(rta[0])
+
+###############################################################################
+
+def LeerContador():
+    contador = comm.getCounter()
+    try:
+        print(" El valor de contador es :",contador[0][8::], "\n\r El estado es:",contador[1])
+    except:
+        print(contador)
+###############################################################################
+def SentidoGiro():
+
+    giro= input('Ingrese sentido de giro\n 1 para ForWare \n 2 para BackWare\n')
+    if (giro == '1'):
+        sentido = comm.setCounterForWard()
+        print(sentido[0])
+    elif (giro == '2'):
+            sentido = comm.setCounterBackWard()
+            print(sentido[0])
+    else:
+        print('Opcion ingresada invalida')
+
+###############################################################################
+def LeerAlturaDrw():
+    altura = comm.getHeightDrw()
+    try:
+        print("Altura:",(altura[2][:-5]), "cm")
+    except:
+        print(altura)
+###############################################################################
+def LeerParametrosDrw():
+    parametros = comm.getParametersDrw()
+    try:
+        ppr = int(parametros[0]) / 4
+        print("PPR: ",ppr, '\nDiametro Tambor:', parametros[1][:-2],'\nDiametro Cable:', parametros[2][:-2],
+              '\nHiladas por capas:', parametros[3],'\nFactor de Aparejo:', parametros[4][:-3], '\nEstado:', parametros[5])
+    except:
+        print(parametros)
+###############################################################################
+def LeerAlturaLin():
+    altura = comm.getHeightLin()
+    try:
+        print("Altura:",(altura[2][:-5]), "cm")
+    except:
+        print(altura)
+###############################################################################
+def LeerParametrosLin():
+    parametros = comm.getParametersLin()
+    try:
+        m = float(parametros[3]) * 4
+        print("Punto 1: ", parametros[0][:-5], '\nPendiente:', str(m), '\nEstado:', parametros[4])
+    except:
+        print('Pendiente no es un numero')
+
+
+###############################################################################
+def hora():
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%d/%m/%Y %H:%M:%S')
+    return st
+
+###############################################################################
+
+def EnviarPuntos():
+    EnviarPunto1()
+    EnviarPunto2()
+
+
+###############################################################################
+
+
+def error():
+   print("Opción no valida")
+
+###############################################################################
+
+comm = 0
+
+lineal = 0
+comm = Comunicacion('192.168.0.245')
+
+
+
+
+#response =ping('192.168.0.245', timeout=10)
+
+
+
+
+#contador =requests.get('http://192.168.0.245/getCounter()')
+#response = requests.get('http://www.aprenderpython.net')
+
+
+dic ={
+    '1': LeerAlturaLin,
+    '2': LeerParametrosLin,
+    '3': LeerAlturaDrw,
+    '4': LeerParametrosDrw,
+    '5': LeerContador,
+    '6': SentidoGiro,
+    '7': EnviarGeometria,
+    '8': EnviarAjuste,
+    '9': EnviarPyM,
+    '10': EnviarPuntos
+    }
+while True:
+    opcion = input("\n\nIngrese una opción\n"
+                   "1 Leer Altura Linial\n"
+                  "2 Leer Parametros Lineal\n"
+                   "3 Leer Altura Drawork\n"
+                  "4 Leer Parametros  Drawork\n"
+                  "5 Leer Contador\n"
+                  "6 Establecer sentido de giro\n"
+                  "7 Enviar Geometria\n"
+                  "8 Enviar Ajuste\n"
+                  "9 Enviar Punto y Pendiente\n"
+                  "10 Enviar 2 puntos\n"
+                  "s Salir\n")
+
+    if(opcion=='s'):
+        break
+    dic.get(opcion, error)()
+
+    conexion = comm.get_conectado()
+    if (conexion == 1):
+        print('CONECTADO')
+    else:
+        print('DESCONECTADO')
---
+++ comunicacion.py
@@ -0,0 +1,196 @@
+
+import requests         #libreria para hhtp
+import re
+
+###############################################################################
+#Clase encargada de la comunicacion HTTP con el K64                           #
+###############################################################################
+class Comunicacion():
+
+    server_address = ('192.168.0.245')
+    conectado = 0
+
+###############################################################################
+# Método constructor de clase. Establece IP                                    #
+###############################################################################
+
+    def __init__(self, address):
+        self.server_address = address
+
+###############################################################################
+# Método que establece IP que tiene el K64 para la comunicación               #
+###############################################################################
+
+    def setAddress(self, address):
+        self.server_address = address
+
+###############################################################################
+# Método que lee el atributo "Conectado". Para saber si hay conexión          #
+###############################################################################
+    def get_conectado(self):
+        return self.conectado
+
+###############################################################################
+# Método que setea el atributo "Conectado".                                   #
+###############################################################################
+    def set_conectado(self, valor):
+        self.conectado = valor
+
+###############################################################################
+# Método que parsea el cuerpo HTML recibido. Para sacar las etiquetas HTML    #
+###############################################################################
+
+    def parse_body(self, body):
+        cleanr = re.compile('<.*?>')
+        body = re.sub(cleanr, '', body)     # Quita las etiquetas HTML dentro de < >
+        return body.split(";")              # retorna el cuempo convertido en lista separada por ";"
+
+###############################################################################
+# Método que envia comando HTML al K64. El comando viene en la varible http   #
+# retorna el texto HTML de respuesta del K64                                  #
+###############################################################################
+    def enviarHTTP(self, http):
+        try:                                        #Utiliza exepcion por si no hay conexion
+            body = requests.get(http, timeout=0.5)  #Envia comando HTTP y genera una exepcion de timeout de 0.5 seg
+            self.conectado = 1
+            return body.text
+
+        except:                                     # Error de conexión
+            print("problemas de conexion")
+            self.conectado = 0
+            return "Error de conexion"
+
+###############################################################################
+# Método que solicita al K64 el valor del contador del Encoder en cuadratura  #
+# Retorna el valor del contador y el status del encoder en una lista          #
+###############################################################################
+    def getCounter(self):
+        http = 'http://{}/getCounter()'.format(self.server_address)
+        print(http)
+        body = self.enviarHTTP(http)
+        datos = self.parse_body(body)
+        return datos
+
+
+###############################################################################
+# Método que envia el sentido de giro ForWare del encoder al K64              #
+# Retorna la respuesta del k64 en una lista                                   #
+###############################################################################
+    def setCounterForWard(self):
+        http = 'http://{}/setCounterForWard()'.format(self.server_address)
+        print(http)
+        body = self.enviarHTTP(http)
+        datos = self.parse_body(body)
+        return datos
+
+###############################################################################
+# Método que envia el sentido de giro BackWare del encoder al K64             #
+# Retorna la respuesta del k64 en una lista                                   #
+###############################################################################
+    def setCounterBackWard(self):
+        http = 'http://{}/setCounterBackWard()'.format(self.server_address)
+        print(http)
+        body = self.enviarHTTP(http)
+        datos = self.parse_body(body)
+        return datos
+
+
+###############################################################################
+# Método que envia solicitud de altura Drawork                                #
+# Retorna la respuesta del k64 con el valor de la altura en una lista         #
+###############################################################################
+    def getHeightDrw(self):
+        http = 'http://{}/getHeightDrw()'.format(self.server_address)
+        body = self.enviarHTTP(http)
+        datos = self.parse_body(body)
+        return datos
+
+###############################################################################
+# Método que envia solicitud de altura en configuracion Lineal del encoder    #
+# Retorna la respuesta del k64 con el valor de la altura en una lista         #
+###############################################################################
+    def getHeightLin(self):
+        http = 'http://{}/getHeightLin()'.format(self.server_address)
+        body = self.enviarHTTP(http)
+        datos = self.parse_body(body)
+        return datos
+
+###############################################################################
+# Método que envia solicitud de los parametros de calibracion del drawork     #
+# Retorna la respuesta del k64 con los valores en una lista                   #
+###############################################################################
+    def getParametersDrw(self):
+        http = 'http://{}/getParametersDrw()'.format(self.server_address)
+        body = self.enviarHTTP(http)
+        datos = self.parse_body(body)
+        return datos
+
+###############################################################################
+# Método que envia solicitud de los parametros de calibracion lineal          #
+# Retorna la respuesta del k64 con los valores en una lista                   #
+###############################################################################
+    def getParametersLin(self):
+        http = 'http://{}/getParametersLin()'.format(self.server_address)
+        body = self.enviarHTTP(http)
+        datos = self.parse_body(body)
+        return datos
+
+###############################################################################
+# Método que envia los parametros geometrico de calibracion del drawork       #
+# Retorna la respuesta del k64 en una lista                   #
+###############################################################################
+    def setGeometry(self, Ppr, DiametroTambor, DiametroCable, Hpc, FactorAparejo):
+
+        http = 'http://{}/setGeometry({},{},{},{},{})'.format(self.server_address,Ppr,DiametroTambor,
+                                                              DiametroCable, Hpc, FactorAparejo)
+        print (http)
+        body = self.enviarHTTP(http)
+        datos = self.parse_body(body)
+        return datos
+
+###############################################################################
+# Método que envia los parametros de calibracion inicial del drawork          #
+# Retorna la respuesta del k64 en una lista                                   #
+###############################################################################
+    def adjust(self, CapasCompletas, Huc, AltRef):
+        http = 'http://{}/adjust({},{},{})'.format(self.server_address,CapasCompletas, Huc, AltRef)
+        print(http)
+        body = self.enviarHTTP(http)
+        datos = self.parse_body(body)
+        return datos
+
+###############################################################################
+# Método que envia el punto de referencia de calibracion para la configuracion#
+# lineal del encoder, se utiliza en el en la calibracion por dos punto y en la#
+# calibracion de punto y pendiente. Retorna la respuesta del k64 en una lista #                                  #
+###############################################################################
+    def setRef2(self, altRef2):
+        http = 'http://{}/setRef2({})'.format(self.server_address,altRef2)
+        print(http)
+        body = self.enviarHTTP(http)
+        datos = self.parse_body(body)
+        return datos
+
+################################################################################
+# Método que enviala pendiente de calibracion para la configuracion lineal con #
+#la calibración de punto y pendiente. Retorna la respuesta del k64 en una lista#
+################################################################################
+    def setM(self, m):
+        http = 'http://{}/setM({})'.format(self.server_address,m)
+        print(http)
+        body = self.enviarHTTP(http)
+        datos = self.parse_body(body)
+        return datos
+
+###############################################################################
+# Método que envia el punto de referencia de calibracion para la configuracion#
+# lineal del encoder, se utiliza en el en la calibracion por dos punto.       #
+# Retorna la respuesta del k64 en una lista                                   #
+###############################################################################
+    def setRef1(self, altRef1):
+        http = 'http://{}/setRef1({})'.format(self.server_address,altRef1)
+        print(http)
+        body = self.enviarHTTP(http)
+        datos = self.parse_body(body)
+        return datos
+
